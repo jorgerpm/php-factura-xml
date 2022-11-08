7a8e9e28e820106a4bb8394abbf40e408286a84b
jorgerpm <jorgepm.4382@gmail.com>
2022-11-08 12:55:13 -0500

    arreglos

--- Utils/constantesUtil.php
+++ Utils/constantesUtil.php
@@ -2,7 +2,6 @@
 
 class constantesUtil {
     public static $TIEMPO_SESION = 180; //3 minutos
-    public static $URL_WEB_SERVICE = "http://192.168.1.34:8080/ServiciosWeb/ideb/";
     public static $URL_WEB_SERVICE = "http://localhost:8080/ServiciosWeb/ideb/";
     public static $URL_ARCHIVOS = 'http://localhost:9090/php-factura-xml/Archivos_subidos';
     
